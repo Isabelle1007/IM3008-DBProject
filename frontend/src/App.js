b9ba6c26e67b8785c98c115c8bb1e90e9edbc944
brandon9912 <b08705028@ntu.edu.tw>
2021-11-03 23:51:21 +0800

    basic header added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import SearchPage from './components/search_page'
+import Header from './components/header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <SearchPage/>
     </div>
   );
